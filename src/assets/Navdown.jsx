8d751e024b136cfb8d2e0bb8d97f1212468e9573
ahmedabdelraouf12 <abdelraoufa170@gmail.com>
2026-04-03 14:26:07 +0200

    sec commit

--- src/assets/Navdown.jsx
+++ src/assets/Navdown.jsx
@@ -8,13 +8,13 @@ import {
 
 export default function Navdown() {
   return (
-    <nav>
-      <div className="d-flex gap-3 bg-black" style={{ padding: "0px 144px 160px 144px" }}>
-        <FontAwesomeIcon icon={faTwitter} className="text-white fs-5" />
-        <FontAwesomeIcon icon={faFacebookF} className="text-white fs-5" />
-        <FontAwesomeIcon icon={faTiktok} className="text-white fs-5" />
-        <FontAwesomeIcon icon={faInstagram} className="text-white fs-5" />
-      </div>
-    </nav>
+    // <nav>
+    //   <div className="d-flex gap-3 bg-black" style={{ padding: "0px 144px 160px 144px" }}>
+    //     <FontAwesomeIcon icon={faTwitter} className="text-white fs-5" />
+    //     <FontAwesomeIcon icon={faFacebookF} className="text-white fs-5" />
+    //     <FontAwesomeIcon icon={faTiktok} className="text-white fs-5" />
+    //     <FontAwesomeIcon icon={faInstagram} className="text-white fs-5" />
+    //   </div>
+    // </nav>
   );
 }
